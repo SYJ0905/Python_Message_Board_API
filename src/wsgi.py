3adac326fb70dcf697bc8e5fc748fd053cc5621e
Cloud <BetaPhoenixSNSD@gmail.com>
2023-08-06 16:44:28 +0800

    feat.add init db

--- src/wsgi.py
+++ src/wsgi.py
@@ -3,6 +3,9 @@ import sys
 
 sys.path.insert(0, os.getcwd())
 
-from src import create_app
+from src import create_app, db
+
 
 application = create_app(config_name="production")
+with application.app__context():
+db.create_all()
